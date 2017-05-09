b45043012c57f455ac800bb4981e262fdab115e2
amolrupnar <crashamol@gmail.com>
2017-05-09 18:46:51 +0530

    ui connected with wheel rig

--- ui/car_win.py
+++ ui/car_win.py
@@ -4,6 +4,7 @@ from shiboken import wrapInstance
 from PySide import QtGui
 from carRig.ui import car_ui
 from carRig.ui import fill_ui_elem
+from carRig.mods import wheel
 
 reload(car_ui)
 
@@ -27,6 +28,27 @@ class CarRig(QtGui.QMainWindow, car_ui.Ui_MainWindow):
         self.bk_wh_L_in_btn.clicked.connect(lambda: fill_ui_elem.filLineEdit(self.bk_wh_L_in_LE))
         self.bk_wh_R_out_btn.clicked.connect(lambda: fill_ui_elem.filLineEdit(self.bk_wh_R_out_LE))
         self.bk_wh_R_in_btn.clicked.connect(lambda: fill_ui_elem.filLineEdit(self.bk_wh_R_in_LE))
+        # create button command add
+        self.create_btn.clicked.connect(self.createRig)
+
+    def createRig(self):
+        # with fill_ui_elem.UndoChunkOpen('wheel rig'):
+        # front wheel left rig.
+        fr_wh_L_out = self.fr_wh_L_out_LE.text()
+        fr_wh_L_in = self.fr_wh_L_in_LE.text()
+        wheel.addStretchRig(fr_wh_L_in, fr_wh_L_out, 'FrontWheelAxel', '_L', 'Main')
+        # front wheel right rig.
+        fr_wh_R_out = self.fr_wh_R_out_LE.text()
+        fr_wh_R_in = self.fr_wh_R_in_LE.text()
+        wheel.addStretchRig(fr_wh_R_in, fr_wh_R_out, 'FrontWheelAxel', '_R', 'Main')
+        # Back wheel left rig.
+        bk_wh_L_out = self.bk_wh_L_out_LE.text()
+        bk_wh_L_in = self.bk_wh_L_in_LE.text()
+        wheel.addStretchRig(bk_wh_L_in, bk_wh_L_out, 'BackWheelAxel', '_L', 'Main')
+        # Back wheel right rig.
+        bk_wh_R_out = self.bk_wh_R_out_LE.text()
+        bk_wh_R_in = self.bk_wh_R_in_LE.text()
+        wheel.addStretchRig(bk_wh_R_in, bk_wh_R_out, 'BackWheelAxel', '_R', 'Main')
 
 
 def main():
--- ui/fill_ui_elem.py
+++ ui/fill_ui_elem.py
@@ -11,3 +11,14 @@ def filLineEdit(lineEdit, sel=None):
     if not sel:
         sel = pm.ls(sl=True)
     lineEdit.setText(str(sel[0]))
+
+
+class UndoChunkOpen(object):
+    def __init__(self, chunkName=''):
+        self.chunkName = chunkName
+
+    def __enter__(self):
+        pm.undoInfo(cn=self.chunkName, openChunk=True)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pm.undoInfo(cn=self.chunkName, closeChunk=True)
