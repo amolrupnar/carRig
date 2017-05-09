c15a56f24e8c83f8b8cf4ff229a9cd5ebf3cf89e
amolrupnar <crashamol@gmail.com>
2017-05-09 17:48:36 +0530

    HandStretch

---
+++ mods/handStretch.py
@@ -0,0 +1,55 @@
+import pymel.core as pm
+import maya.OpenMaya as om
+
+
+def handStretch(start_ctl, end_ctl, maxPivot, jointStretchy, mainController='Main', name='HandStretch', side='_L'):
+    """
+    add stretch in straight hand.
+    :param start_ctl: string
+    :param end_ctl: string
+    :param maxPivot: string
+    :param jointStretchy: string
+    :param mainController: string
+    :param name: string
+    :param side: string
+    :return: stretchNodes
+    """
+    # convert all inputs in to PyNode.
+    start_ctl = pm.PyNode(start_ctl)
+    end_ctl = pm.PyNode(end_ctl)
+    maxPivot = pm.PyNode(maxPivot)
+    jointStretchy = pm.PyNode(jointStretchy)
+    mainController = pm.PyNode(mainController)
+
+    # add stretch with main controller.
+    loc_st = pm.spaceLocator(n='LocDistanceStart_' + name + side)
+    loc_end = pm.spaceLocator(n='LocDistanceEnd_' + name + side)
+    pm.pointConstraint(start_ctl, loc_st)
+    pm.pointConstraint(end_ctl, loc_end)
+    distNode = pm.createNode('distanceBetween', n='distanceBetween' + name + side, ss=True)
+    loc_st.t.connect(distNode.point1)
+    loc_end.t.connect(distNode.point2)
+    # get max Stretch.
+    obj1 = pm.xform(start_ctl, q=True, ws=True, t=True)
+    obj2 = pm.xform(maxPivot, q=True, ws=True, t=True)
+    posX = obj1[0] - obj2[0]
+    posY = obj1[1] - obj2[1]
+    posZ = obj1[2] - obj2[2]
+    w_dist = om.MVector(posX, posY, posZ)
+    maxDistance = w_dist.length()
+    # createNode.
+    mdn_main_scale = pm.createNode('multiplyDivide', n='multiplyDivide' + name + 'MainScale' + side, ss=True)
+    mdn_stretch = pm.createNode('multiplyDivide', n='multiplyDivide' + name + side, ss=True)
+    mainController.sy.connect(mdn_main_scale.input1X)
+    mdn_main_scale.input2X.set(maxDistance)
+    mdn_main_scale.operation.set(1)
+    mdn_stretch.operation.set(2)
+    distNode.distance.connect(mdn_stretch.input1X)
+    mdn_main_scale.outputX.connect(mdn_stretch.input2X)
+    # create condition node.
+    cond_node = pm.createNode('condition', n='condition' + name + side, ss=True)
+    cond_node.operation.set(2)
+    distNode.distance.connect(cond_node.firstTerm)
+    mdn_main_scale.outputX.connect(cond_node.secondTerm)
+    mdn_stretch.outputX.connect(cond_node.colorIfTrueR)
+    cond_node.outColorR.connect(jointStretchy.sx)
--- mods/wheel.py
+++ mods/wheel.py
@@ -1,18 +1,14 @@
 import pymel.core as pm
 
-startPivot = 'FKFrontWheelRod_L'
-endPivot = 'FKfrontWheel_L'
-naming = 'FrontWheelAxel'
-side = '_L'
 
-
-def addStretchRig(start_ctl, end_ctl, name, ctl_side):
+def addStretchRig(start_ctl, end_ctl, name, ctl_side, main_ctl):
     """
     add wheel stretch with connecting rod rig module.
     :param start_ctl: string
     :param end_ctl: string
     :param name: string
     :param ctl_side: string
+    :param main_ctl: string
     :return: wheel_rig
     """
     # axel start joint.
@@ -35,15 +31,34 @@ def addStretchRig(start_ctl, end_ctl, name, ctl_side):
     ikHandle = pm.ikHandle(sj=st_axel_jt, ee=end_axel_jt, sol='ikRPsolver', n='IK' + name + ctl_side)
     pm.pointConstraint(end_ctl, ikHandle[0], mo=True)
     # add stretch with main controller.
-    loc_st = pm.spaceLocator(n='LocDistanceStart_' + name + side)
-    loc_end = pm.spaceLocator(n='LocDistanceEnd_' + name + side)
+    loc_st = pm.spaceLocator(n='LocDistanceStart_' + name + ctl_side)
+    loc_end = pm.spaceLocator(n='LocDistanceEnd_' + name + ctl_side)
     pm.pointConstraint(st_axel_jt, loc_st)
     pm.pointConstraint(end_ctl, loc_end)
     distNode = pm.createNode('distanceBetween', n='distanceBetween' + name, ss=True)
     loc_st.t.connect(distNode.point1)
     loc_end.t.connect(distNode.point2)
+    mdn_main_scale = pm.createNode('multiplyDivide', n='multiplyDivideMainScale_' + name + ctl_side)
+    mdn_main_scale.operation.set(2)
+    distNode.distance.connect(mdn_main_scale.input1X)
+    pm.connectAttr(main_ctl + '.scaleY', mdn_main_scale.input2X, f=True)
     mdn = pm.createNode('multiplyDivide', n='multiplyDivide_' + name + ctl_side, ss=True)
     mdn.operation.set(2)
-    distNode.distance.connect(mdn.input1X)
+    mdn_main_scale.outputX.connect(mdn.input1X)
     mdn.input2X.set(distNode.distance.get())
     mdn.outputX.connect(st_axel_jt.sx)
+    # add skinning joints.
+    pm.select(cl=True)
+    skinning_joint_start = pm.joint(n=name + '_StartSkinJoint' + ctl_side)
+    pm.select(cl=True)
+    skinning_joint_end = pm.joint(n=name + '_EndSkinJoint' + ctl_side)
+    pm.delete(pm.parentConstraint(start_ctl, skinning_joint_start))
+    pm.delete(pm.parentConstraint(end_ctl, skinning_joint_end))
+    # parent skin joints.
+    pm.parent(skinning_joint_start, st_axel_jt)
+    pm.parent(skinning_joint_end, end_axel_jt)
+    # aim constraint skin joints.
+    pm.aimConstraint(end_ctl, skinning_joint_start, o=[0, 0, 0], w=True, aim=[1, 0, 0], u=[0, 1, 0], wut="vector",
+                     wu=[0, 1, 0], mo=False)
+    pm.aimConstraint(start_ctl, skinning_joint_end, o=[0, 0, 0], w=True, aim=[1, 0, 0], u=[0, 1, 0], wut="vector",
+                     wu=[0, 1, 0], mo=False)
